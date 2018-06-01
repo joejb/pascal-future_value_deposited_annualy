860acd267f54192b8f519f5a0ee33010e596c01f
joejb <joebh@hotmail.com>
2018-05-31 23:00:06 -0400

    Update annuity.pas

--- annuity.pas
+++ annuity.pas
@@ -1,10 +1,23 @@
-program FutureVal(input, output);
+begin {main}
+	writeln ('Future value of an investment');
+	
+	repeat
+		writeln;
+		write('Initial Investment: $');
+		readln(investment);
+		write('Nominal Interest rate (%) ');
+		readln(percent);
+		write('Number of compounding periods per year: ');
+		readln(NumPeriods);
+		write('Number of whole year: ');
+		readln(NumYears);
+		write('Number of periods past last whole years: ');
+		readln(XtraPeriods);
 
-var
-  NumPeriods, XtraPeriods, NumYears : integers;
-  investment, percent, rates : real;
-
-{$I IntRaise}
-{$I NotAgain}
-
-begin
+		rate:= percent / NumPeriods / 100;
+		writeln('Future value = $', 
+				investment * IntRaise (1 + rate, NumPeriods * NumYears + XtraPeriods)
+				:9:2);
+		writeln
+	until NotAgain
+end.
