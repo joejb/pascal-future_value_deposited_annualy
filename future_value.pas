a5cc1916268231ebc69f2ea1f53a8ad73543b3c2
joejb <joebh@hotmail.com>
2018-06-02 07:39:56 -0400

    Update future_value.pas

--- future_value.pas
+++ future_value.pas
@@ -1,1 +1,23 @@
+begin {main}
+	writeln ('Future value of an investment');
+	
+	repeat
+		writeln;
+		write('Initial Investment: $');
+		readln(investment);
+		write('Nominal Interest rate (%) ');
+		readln(percent);
+		write('Number of compounding periods per year: ');
+		readln(NumPeriods);
+		write('Number of whole year: ');
+		readln(NumYears);
+		write('Number of periods past last whole years: ');
+		readln(XtraPeriods);
 
+		rate:= percent / NumPeriods / 100;
+		writeln('Future value = $', 
+				investment * IntRaise (1 + rate, NumPeriods * NumYears + XtraPeriods)
+				:9:2);
+		writeln
+	until NotAgain
+end.
